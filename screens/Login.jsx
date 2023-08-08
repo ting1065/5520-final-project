e80068a3c2ef876948f1754da2316ef3fbbb23ae
ting1065 <fengtingtang1065@gmail.com>
2023-08-08 03:56:18 -0700

    finished the prototype of login/signup

--- screens/Login.jsx
+++ screens/Login.jsx
@@ -1,10 +1,46 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, TextInput, Alert } from 'react-native'
+import React,  { useState } from 'react'
+import PressableButton from '../components/PressableButton'
+import { auth } from "../Firebase/firebase-setup";
+import { signInWithEmailAndPassword } from "firebase/auth";
 
-export default function Login() {
+export default function Login({ navigation }) {
+
+  //regex for email validation
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function loginHandler() {
+
+    if (!emailRegex.test(email)) {
+      Alert.alert("Please enter a valid email address");
+      return;
+    }
+
+    try {
+      const user = await signInWithEmailAndPassword(auth, email, password);
+    } catch (error) {
+      console.log("error happened while logging in:",error);
+      Alert.alert("error happened while logging in:",error);
+    }
+  }
+
+
   return (
     <View>
-      <Text>Login</Text>
+      <Text>Email Address</Text>
+      <TextInput autoCapitalize="none" placeholder="Email" value={email} onChangeText={(newText)=>setEmail(newText)}/>
+      <Text>Password</Text>
+      <TextInput autoCapitalize="none" placeholder="Password" secureTextEntry={true} value={password} onChangeText={(newText)=>setPassword(newText)}/>
+      <PressableButton onPress={()=>loginHandler()}>
+        <Text>Log In</Text>
+      </PressableButton>
+      <Text>New User?</Text>
+      <PressableButton onPress={()=>{navigation.replace("Signup")}}>
+        <Text>Create an account</Text>
+      </PressableButton>
     </View>
   )
 }
--- screens/Signup.jsx
+++ screens/Signup.jsx
@@ -1,10 +1,58 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, TextInput, Alert } from 'react-native'
+import React, { useState } from 'react'
+import PressableButton from '../components/PressableButton'
+import { auth } from "../Firebase/firebase-setup";
+import { createUserWithEmailAndPassword } from "firebase/auth";
 
-export default function Signup() {
+export default function Signup({ navigation }) {
+
+  //regex for email validation
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmedPassword, setConfirmedPassword] = useState("");
+
+  async function signupHandler() {
+
+    if (!emailRegex.test(email)) {
+      Alert.alert("Please enter a valid email address");
+      return;
+    }
+
+    if (password !== confirmedPassword) {
+      Alert.alert("Passwords do not match");
+      return;
+    }
+
+    if (password.length < 6) {
+      Alert.alert("Password should be at least 6 characters");
+      return;
+    }
+
+    try {
+      await createUserWithEmailAndPassword(auth, email, password);
+    } catch (error) {
+      console.log("error happened while signing up:",error);
+      Alert.alert("error happened while signing up:",error);
+    }
+  }
+
   return (
     <View>
-      <Text>Signup</Text>
+      <Text>Email Address</Text>
+      <TextInput autoCapitalize="none" placeholder="Email" value={email} onChangeText={(newText)=>setEmail(newText)}/>
+      <Text>Password</Text>
+      <TextInput autoCapitalize="none" placeholder="Password" secureTextEntry={true} value={password} onChangeText={(newText)=>setPassword(newText)}/>
+      <Text>Confirm Password</Text>
+      <TextInput autoCapitalize="none" placeholder="Password" secureTextEntry={true} value={confirmedPassword} onChangeText={(newText)=>setConfirmedPassword(newText)}/>
+      <PressableButton onPress={()=>signupHandler()}>
+        <Text>Register</Text>
+      </PressableButton>
+      <Text>Already Registered?</Text>
+      <PressableButton onPress={()=>navigation.replace("Login")}>
+        <Text>Login</Text>
+      </PressableButton>
     </View>
   )
 }
