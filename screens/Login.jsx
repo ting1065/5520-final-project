aefbc72353cd9e99f403912963fe81d608859ef9
HunterLL <hunter_law@outlook.com>
2023-08-15 21:01:58 -0700

    included login info in a card

--- screens/Login.jsx
+++ screens/Login.jsx
@@ -3,6 +3,8 @@ import React, { useState } from "react";
 import PressableButton from "../components/PressableButton";
 import { auth } from "../Firebase/firebase-setup";
 import { signInWithEmailAndPassword } from "firebase/auth";
+import Card from "../components/Card";
+import { colors } from '../Colors'
 
 export default function Login({ navigation }) {
   //regex for email validation
@@ -35,25 +37,32 @@ export default function Login({ navigation }) {
       <Text>Welcome</Text>
       <Text>Back!</Text>
       <Text>Login back into your account</Text>
-      <Text>Email Address</Text>
-      <TextInput
-        autoCapitalize="none"
-        placeholder="Email"
-        value={email}
-        onChangeText={(newText) => setEmail(newText)}
-      />
-      <Text>Password</Text>
-      <TextInput
-        autoCapitalize="none"
-        placeholder="Password"
-        secureTextEntry={true}
-        value={password}
-        onChangeText={(newText) => setPassword(newText)}
-      />
-      <Text>=======</Text>
-      <PressableButton onPress={() => loginHandler()}>
-        <Text>Log In</Text>
-      </PressableButton>
+      <Card 
+        width={300}
+        height={300}
+        backgroundColor={'white'}
+      >
+        <Text>Email Address</Text>
+        <TextInput
+          autoCapitalize="none"
+          placeholder="Email"
+          value={email}
+          onChangeText={(newText) => setEmail(newText)}
+        />
+        <Text>Password</Text>
+        <TextInput
+          autoCapitalize="none"
+          placeholder="Password"
+          secureTextEntry={true}
+          value={password}
+          onChangeText={(newText) => setPassword(newText)}
+        />
+        <Text>=======</Text>
+        <PressableButton onPress={() => loginHandler()}>
+          <Text>Log In</Text>
+        </PressableButton>
+      </Card>
+        
       <Text>=======</Text>
       <Text>New User?</Text>
       <Text>=======</Text>
