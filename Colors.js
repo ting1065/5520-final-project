547361b2826a1cf17698c8c76eb6ce2ca736edb1
HunterLL <hunter_law@outlook.com>
2023-08-15 22:31:27 -0700

    tested&changed some gradient color sets

--- Colors.js
+++ Colors.js
@@ -1,12 +1,29 @@
 export const colors = {
     main: 'darkblue',
-    gradient1: '#87ceeb',
-    gradient2: '#4682b4',
-    gradient3: '#6a5acd',
     redButton: "crimson",
     blueButton: '#1e90ff',
     cardBackground: '#a9a9a9',
     shadowColor: '#000',
+    //gradient test set 1
+    // gradient1: '#87ceeb',
+    // gradient2: '#4682b4',
+    // gradient3: '#6a5acd',
+    //gradient test set 2
+    // gradient1: '#F99B7D',
+    // gradient2: '#E76161',
+    // gradient3: '#B04759',
+    //gradient test set 3
+    // gradient1: '#B9EDDD',
+    // gradient2: '#87CBB9',
+    // gradient3: '#569DAA',
+    //gradient test set 4
+    gradient1: '#F7D060',
+    gradient2: '#F3E99F',
+    gradient3: '#98D8AA',
+    //gradient test set 5
+    // gradient1: '#CECE5A',
+    // gradient2: '#FFE17B',
+    // gradient3: '#FD8D14',
 
 };
 
