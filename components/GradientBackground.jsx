0c709bffd3624452036b015df6d1076a4708e2bd
HunterLL <hunter_law@outlook.com>
2023-08-16 12:32:48 -0700

    created linear gradient component for background

---
+++ components/GradientBackground.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import {LinearGradient}  from 'expo-linear-gradient';
+import { colors } from '../Colors';
+ 
+const GradientBackground = ({ children }) => {
+    return (
+      <LinearGradient
+        colors={[colors.gradient1, colors.gradient2, colors.gradient3]}
+        style={[StyleSheet.absoluteFill, styles.container]}
+      >
+        {children}
+      </LinearGradient>
+    );
+};
+  
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+    },
+});
+  
+export default GradientBackground;
