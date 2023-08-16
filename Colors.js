090606947653ac5fbedf2cdafb4b3099d7ee3d5a
HunterLL <hunter_law@outlook.com>
2023-08-15 17:01:48 -0700

    created Colors file

---
+++ Colors.js
@@ -0,0 +1,13 @@
+export const colors = {
+    main: 'darkblue',
+    gradient1: '#87ceeb',
+    gradient2: '#4682b4',
+    gradient3: '#6a5acd',
+    redButton: "crimson",
+    blueButton: '#1e90ff',
+    cardBackground: '#a9a9a9',
+    shadowColor: '#000',
+
+};
+
+// import { colors } from './colors.js';
