bd52416975e5d05c902ce0ee7329c6db2feb7c0c
ting1065 <fengtingtang1065@gmail.com>
2023-08-18 20:38:44 -0700

    make list mode show players' rank and score

--- components/PlayerInList.jsx
+++ components/PlayerInList.jsx
@@ -2,62 +2,129 @@ import { Text, Image, StyleSheet, View } from "react-native";
 import React from "react";
 import PressableButton from "./PressableButton";
 import { auth } from "../Firebase/firebase-setup";
-import { colors } from '../Colors';
+import { colors } from "../Colors";
 
 export default function PlayerInList({ clickHandler, player }) {
   return (
-      <PressableButton 
-        defaultStyle={styles.playerDefaultStyle}
-        pressedStyle={styles.playerPressedStyle}
-        onPress={async () => await clickHandler(player)}
-      >
-        <View style={styles.playerContainer}>
-          <Image
-          style={styles.image}
-          source={{ uri: player.avatar }}
-        />
-        
-          {auth.currentUser.uid === player.id ? <Text style={styles.selfName}>{player.name}</Text>: <Text style={styles.playerName}>{player.name}</Text>}
+    <PressableButton
+      defaultStyle={styles.playerDefaultStyle}
+      pressedStyle={styles.playerPressedStyle}
+      onPress={async () => await clickHandler(player)}
+    >
+      <View style={styles.playerContainer}>
+        <View style={styles.leftWrapper}>
+          <View style={styles.nameWrapper}>
+            <Text
+              style={
+                auth.currentUser.uid === player.id
+                  ? styles.selfName
+                  : styles.playerName
+              }
+            >
+              {player.name}
+            </Text>
+          </View>
+
+          <View style={styles.imageWrapper}>
+            <Image style={styles.image} source={{ uri: player.avatar }} />
+          </View>
         </View>
-        
-      </PressableButton>
+
+        <View style={styles.rightWrapper}>
+          <View style={styles.rightTextWrapper}>
+          <Text
+            style={
+              auth.currentUser.uid === player.id
+                ? styles.selfStat
+                : styles.playerStat
+            }
+          >
+            Rank: {player.rank}
+          </Text>
+          <Text
+            style={
+              auth.currentUser.uid === player.id
+                ? styles.selfStat
+                : styles.playerStat
+            }
+          >
+            Score: {player.score}
+          </Text>
+          </View>
+        </View>
+      </View>
+    </PressableButton>
   );
 }
 
 const styles = StyleSheet.create({
-  image: {
-    width: 70, 
-    height: 70,
-    resizeMode: 'cover',
-    marginRight: 10,
-  
-  },
   playerContainer: {
-    width: '100%',
+    width: "100%",
     height: 90,
     borderRadius: 60,
-    flexDirection: 'row',
-    justifyContent: 'space-evenly',
-    alignItems: 'center',
-    alignSelf: 'center',
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    alignItems: "center",
+    alignSelf: "center",
     // margin: 10,
     backgroundColor: colors.shadowColor,
   },
+  leftWrapper: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "space-evenly",
+  },
+  nameWrapper: {
+    height: "27%",
+    width: "100%",
+  },
+  imageWrapper: {
+    height: "68%",
+    width: "100%",
+  },
+  image: {
+    height: "100%",
+    resizeMode: "contain",
+  },
+  rightWrapper: {
+    flex: 1,
+    height: "100%",
+    alignItems: "center",
+    justifyContent: "space-evenly",
+  },
+  rightTextWrapper: {
+    height: "100%",
+    width: "60%",
+    alignItems: "flex-start",
+    justifyContent: "space-evenly",
+  },
   playerName: {
     color: colors.whiteWords,
-    fontSize: 20,
+    fontSize: 16,
+    textAlign: "center",
   },
   selfName: {
     color: colors.goldWords,
+    fontSize: 16,
+    textAlign: "center",
+  },
+  playerStat: {
+    color: colors.whiteWords,
     fontSize: 20,
+    textAlign: "center",
+  },
+  selfStat: {
+    color: colors.goldWords,
+    fontSize: 20,
+    textAlign: "center",
   },
   playerDefaultStyle: {
-    width: '90%',
+    width: "90%",
     height: 90,
     borderRadius: 60,
-    backgroundColor: 'white',
+    backgroundColor: "white",
     margin: 10,
-    alignSelf: 'center',
+    alignSelf: "center",
     ...Platform.select({
       ios: {
         shadowColor: colors.shadowColor,
@@ -73,5 +140,4 @@ const styles = StyleSheet.create({
   playerPressedStyle: {
     opacity: 0.5,
   },
-
-})
+});
